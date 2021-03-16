e8bdd7e6ca260d6e32c5b7cc415f5b98a625af7d
brenno-calado <brenno.calado@gmail.com>
2021-03-16 11:42:20 -0300

    fix data import

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/container.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/container.jsx
@@ -3,7 +3,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import UserListService from '/imports/ui/components/user-list/service';
 import UserParticipants from './component';
 import { meetingIsBreakout } from '/imports/ui/components/app/service';
-import { data } from '/imports/ui/components/media/container';
+import data from '/imports/ui/components/media/container';
 
 const UserParticipantsContainer = props => <UserParticipants {...props} />;
 
